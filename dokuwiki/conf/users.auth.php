0ca971e89abd124621b4ebee7d0d4db3e5d58ab9
EgoBits1 <egobits1@gmail.com>
2016-07-18 22:53:50 +0200

    Trying new Wiki

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$011aPDXc$I51We3PElU9C6vd0bqmDf.:Ego:egobits1@gmail.com:admin,user
